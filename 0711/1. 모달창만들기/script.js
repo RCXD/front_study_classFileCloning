706bc1e80d039f3b31644f86fcbe84725eb91e7d
ghkdss <sotjstod@gmail.com>
2025-07-11 10:22:57 +0900

    0711 1교시

---
+++ 0711/1. 모달창만들기/script.js
@@ -0,0 +1,23 @@
+const btnBoard = document.querySelector('#open');
+const modalBox = document.querySelector('.black-bg');
+
+// js문법
+// btnBoard.addEventListener( 'click', () => {
+//   modalBox.classList.add('show');
+// } );
+
+// jQuery
+$('#open').on( 'click', () => {
+  $('.black-bg').addClass('show');
+} )
+
+const btnClose = document.querySelector('#close');
+// js
+// btnClose.addEventListener('click', function() {
+//   modalBox.classList.remove('show');
+// });
+
+// jQuery
+$('#close').on('click', ()=>{
+  $('.black-bg').removeClass('show');
+})
